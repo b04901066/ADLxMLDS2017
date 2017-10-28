a68d305fa463b23dd3e8168c6ac19d8f488f3084
洪國喨 <31993733+b04901066@users.noreply.github.com>
2017-10-28 23:42:45 +0800

    Add files via upload

--- hw1/hw1_cnn.sh
+++ hw1/hw1_cnn.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 wget 'https://www.dropbox.com/s/e8smfipin3fesxw/cnn.h5?dl=1' -o './cnn.h5'
-python ./cnn_LSTM_test.py $1 $2 ./cnn.h5
+python ./cnn_LSTM_test.py $1 $2 ./cnn.h5?dl=1
 exit 0
--- hw1/hw1_rnn.sh
+++ hw1/hw1_rnn.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 wget 'https://www.dropbox.com/s/qw4jr3g19surfn6/rnn.h5?dl=1' -o './rnn.h5'
-python ./rnn_LSTM_test.py $1 $2 ./rnn.h5
+python ./rnn_LSTM_test.py $1 $2 ./rnn.h5?dl=1
 exit 0
