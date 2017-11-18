4741486006b11c109f8e3f0deb7bde52868f8d2b
洪國喨 <31993733+b04901066@users.noreply.github.com>
2017-11-18 23:11:58 +0800

    Update hw2_seq2seq.sh

--- hw2/hw2_seq2seq.sh
+++ hw2/hw2_seq2seq.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget 'https://www.dropbox.com/s/9y4lwjv1b1vogh1/seq210.h5?dl=1' -O './seq2seq_model.h5'
+wget 'https://www.dropbox.com/s/j6vcd425xw9xgzi/seq210.h5?dl=1' -O './seq2seq_model.h5'
 python ./test_seq2seq.py $1 $2 ./seq2seq_model.h5 testing_data
 python ./test_seq2seq.py $1 $3 ./seq2seq_model.h5 peer_review
 exit 0
